a0a81cd04f5790376e00ff95345046223bc1ca67
mphanias <121247041+mphanias@users.noreply.github.com>
2023-11-07 11:54:42 +0530

    OM127 - 7.0 node view changes (#76)
    7.0 based theme to combine 6.0 and 7.0 changes while showing state, topk and time-series values

--- tests/processing/extract_queries.py
+++ tests/processing/extract_queries.py
@@ -2,7 +2,7 @@ import json
 import argparse
 import os
 
-def parse_dashboard_json(json_data, output_file, subfolder_name, json_file_name):
+def parse_dashboard_json(json_data, output_filename, subfolder_name, json_file_name):
     data = json.loads(json_data)
 
     dashboard_name = data['title']
@@ -14,72 +14,98 @@ def parse_dashboard_json(json_data, output_file, subfolder_name, json_file_name)
 
     row_title = base_row_title
     row_id = base_row_id
-
-    with open(output_file, 'a') as out_file:
+    
+    with open(output_filename, 'a') as out_file:
+        # both rows and panels are treated as panels in grafana, with type as "row" for rows
+        # print( len(panels))
         for panel in panels:
+            # print ("processing panel : ", panel['type'])
             if 'type' not in panel:
                 continue
-
+            # if json_file_name=="connectorsview.json":
             if panel['type'] == 'row':
                 row_title = panel.get('title', base_row_title)
                 row_id = panel.get('id', base_row_id)
                 index_counter = 0  # Reset index_counter for each panel
-            elif panel['type'] == 'grafana-polystat-panel':
-                # For 'grafana-polystat-panel', do not change the row_title and row_id
-                pass
+                    
+                # this is a row, handle child panels 
+            if "panels" in panel:
+                print(" len(panel[panels]) ", len(panel["panels"]))
+                for p_sub_panel in panel["panels"]:
+                    handle_panel(out_file, subfolder_name, json_file_name, dashboard_name, row_title, row_id, p_sub_panel)                            
+            else: # this is individual panel without a row
+                print("this is a single panel processing")
+                handle_panel(out_file, subfolder_name, json_file_name, dashboard_name, row_title, row_id, panel)
+                    
+                # elif panel['type'] == 'grafana-polystat-panel':
+                #     # For 'grafana-polystat-panel', do not change the row_title and row_id
+                #     pass
+                    
+                
+def handle_panel(p_out_file, p_subfolder_name, p_json_file_name, p_dashboard_name, p_row_title, p_row_id, p_panel):
+    if 'title' in p_panel:
+        panel_title = p_panel['title']
+    else:
+        panel_title = "No Title"
+    # print( " panel title ", panel_title)
+    # print("processing ... p_row_title: ", p_row_title, "\tpanel_title: ",panel_title)
 
-            if 'title' in panel:
-                panel_title = panel['title']
+    if 'id' in p_panel:
+        panel_id = p_panel['id']
+    else:
+        panel_id = "No ID"
+            
+    index_counter = 0
+    # if p_json_file_name=="all_flash.json" or p_json_file_name=="connectorsview.json":
+    #     print("panel_title: ", panel_title)
+    #     print("processing ... json_file_name: ",p_json_file_name," --- ('targets' in panel)? ", ('targets' in p_panel))
+    #     print("'type' in panel ", 'type' in p_panel, "\t panel title:? ", 'title' in p_panel,"\t panel['type'] == 'row': ", p_panel['type'] == 'row')
+            
+    if ('targets' not in p_panel):
+        print(" no targets in the panel... hence ignoring")
+    else:
+        print(" panel_title: ", panel_title, " \t len(p_panel['targets']): ", len(p_panel['targets']))
+        for target in p_panel['targets']:
+
+            refid = "No RefID"
+            expr = "Panel No Expression"
+            
+            if 'expr' in target:
+                expr = target['expr']
             else:
-                panel_title = "No Title"
+                if p_panel['type'] == 'row':
+                    expr = "Row No expression"
 
-            if 'id' in panel:
-                panel_id = panel['id']
-            else:
-                panel_id = "No ID"
-            index_counter = 0
-            if 'targets' in panel:
-                targets = panel['targets']
-                for target in targets:
-                    if 'expr' in target:
-                        expr = target['expr']
-                    else:
-                        if panel['type'] == 'row':
-                            expr = "Row No expression"
-                        else:
-                            expr = "Panel No Expression"
-                    if 'refId' in target:
-                        refid = target['refId']
-                    else:
-                        refid = "No RefID"
-                    entry = {
-                        "folder": subfolder_name,
-                        "dashboard_file": json_file_name,
-                        "dashboard_name": dashboard_name,
-                        "row_title": row_title,
-                        "row_id": str(row_id),
-                        "panel_name": panel_title,
-                        "panel_id": str(panel_id),
-                        "refid": refid,
-                        "index": str(index_counter),
-                        "expr": expr
-                    }
+            if 'refId' in target:
+                refid = target['refId']
+                    
+            entry = {
+                "folder": p_subfolder_name,
+                "dashboard_file": p_json_file_name,
+                "dashboard_name": p_dashboard_name,
+                "row_title": p_row_title,
+                 "row_id": str(p_row_id),
+                "panel_name": panel_title,
+                "panel_id": str(panel_id),
+                "refid": refid,
+                "index": str(index_counter),
+                "expr": expr
+            }
 
-                    # Create the entry dictionary
-                    entry_dict = {
-                        "key": f"{subfolder_name}/{json_file_name}/{dashboard_name}/{row_title}/{row_id}/{panel_title}/{panel_id}/{refid}/{index_counter}",
-                        "entry": entry
-                    }
-                    out_file.write(json.dumps(entry_dict) + "\n")
-                    # result = f"{subfolder_name}/{json_file_name}/{dashboard_name}/{row_title}/{row_id}/{panel_title}/{panel_id}/{refid}/{index_counter} = {expr}".replace('\n', ' ')
-                    # # print(result)
-                    # out_file.write(result + "\n")
-                    index_counter += 1
+            # Create the entry dictionary
+            entry_dict = {
+                "key": f"{p_subfolder_name}/{p_json_file_name}/{p_dashboard_name}/{p_row_title}/{p_row_id}/{panel_title}/{panel_id}/{refid}/{index_counter}",
+                "entry": entry
+            }
+            index_counter += 1
+            p_out_file.write(json.dumps(entry_dict) + "\n")
 
 def parse_files(p_args):
     out_file = open(fn_mock_queries, 'w')
+    
+    print("starting processing of given folder : ", p_args.folder_path)
 
-    for subfolder_name, _, filenames in os.walk(p_args.folder_path):
+    for subfolder_name, dirs, filenames in os.walk(p_args.folder_path):
         for filename in filenames:
             if filename.endswith('.json'):
                 print(" processing .... "+filename)
--- tests/processing/var_replacer.py
+++ tests/processing/var_replacer.py
@@ -17,6 +17,12 @@ def get_dashboard_name(line):
 def apply_replacements(replacements, text_content):
     lines = text_content.split("\n")
     updated_lines = []
+    error_messages = set()
+    list_of_variables = ["$set_quota_topk" ,"$node", "$severity", "$state", "$job_name", "$cluster", "$namespace", "$set", 
+                         "$sindex", "$dc", "${operation}", "$latency_time_bucket", "$job_id", "$operation",
+                         "$xdr_proxy_cluster_name", "$kafka_cluster_name", "$pulsar_cluster_name", 
+                         "$jms_cluster_name", "$elastic_cluster_name", "$esp_cluster_name", "$instance", "$connector_type", 
+                         "$user", "${latencytimeunit}", "$__rate_interval", "$topk_limit"]
 
     for line in lines:
         dashboard_name = get_dashboard_name(line)
@@ -24,13 +30,37 @@ def apply_replacements(replacements, text_content):
         # encode every " with \" for query execution
         # line = line.replace('"', '\'')
         # print( line)
-        
         if dashboard_name in replacements:
-            for placeholder, replacement in replacements[dashboard_name].items():
+            # for placeholder, replacement in replacements[dashboard_name].items():
+            for l_variable in list_of_variables:
+                replacement = fetch_replacement_value( dashboard_name, replacements, l_variable)
+                # promql variable can be like $node or $node|$^
+                line = line.replace("|$^", "")
+                line = line.replace(":pipe", "")
+                line = line.replace(l_variable, replacement)
+        else:
+            error_messages.add ("dashboard: "+dashboard_name +" no over-rides, using default_for_all values")
+            for placeholder, replacement in replacements["default_for_all"].items():
+                # promql variable can be like $node or $node|$^
+                line = line.replace("|$^", "")
+                line = line.replace(":pipe", "")
                 line = line.replace(placeholder, replacement)
+                # print(line)
+            
         updated_lines.append(line)
-
+    print(error_messages)
     return "\n".join(updated_lines)
+
+def fetch_replacement_value( p_dashboard_name, p_replacements, p_variable):
+    for placeholder, replacement in p_replacements[p_dashboard_name].items():
+        if placeholder == p_variable:
+            return replacement
+    # return default 
+    for placeholder, replacement in p_replacements["default_for_all"].items():
+        if placeholder == p_variable:
+            return replacement
+        
+    return p_variable
 
 def replace_variable():
 
