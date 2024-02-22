426a92bce77cdf4820d1a328bf6a5578866a0b3b
Adam Hevenor <ahevenor@aerospike.com>
2024-02-22 14:05:58 -0700

    seven oh

--- examples/aerolab/01-create.sh
+++ examples/aerolab/01-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aerolab cluster create -n dc1 -c 5 --customconf="./aerospike.conf" --toolsconf="./astools.conf" --instance e2-standard-2 --zone us-central1-a --disk pd-balanced:20 --disk pd-ssd:40 --disk pd-ssd:40
+aerolab cluster create -n dc1 -c 1 --customconf="./aerospike.conf" --toolsconf="./astools.conf" --instance e2-standard-2 --zone us-central1-a --disk pd-balanced:20 --disk pd-ssd:40 --disk pd-ssd:40
 aerolab cluster create -n dc2 -c 3 --customconf="./aerospike.conf" --toolsconf="./astools.conf"  --instance  e2-standard-2 --zone us-central1-a --disk pd-balanced:20 --disk pd-ssd:40 --disk pd-ssd:40
 
 
@@ -65,4 +65,5 @@ aerolab cluster add exporter -n dc2 -o ape2.toml
 
 aerolab client create ams -n ams -s dc1,dc2 --instance e2-medium --zone us-central1-a --disk pd-balanced:20 -e 3000:3000
 aerolab client attach  -n ams -- grafana-cli plugins install grafana-polystat-panel
+aerolab client attach  -n ams -- grafana-cli plugins install jdbranham-diagram-panel
 aerolab client attach  -n ams -- service grafana-server restart
